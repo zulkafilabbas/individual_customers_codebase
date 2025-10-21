72955eb2f08b64349af050679ff6b9660634aca7
Zulkafil Abbas <zulkafilabbas@gmail.com>
2025-10-21 16:13:01 +0900

    cleanup

--- test/test_stable_selector3.py
+++ test/test_stable_selector3.py
@@ -1,12 +1,12 @@
 import rerun as rr
 import h5py
-from data_processing.data_extraction.stable_selector2 import StableSelectorV2
+from data_processing.data_extraction.stable_selector2 import StableSelector
 
 path = r"C:\Users\zulkafil\Desktop\individual_customers_codebase\dataset\2025-03-16_s2_merged_tracked.hdf5"
 
 with h5py.File(path, "r") as f_tmp:
     tid = list(f_tmp["poses_raw"].keys())[0]
 
-selector = StableSelectorV2()
+selector = StableSelector()
 with h5py.File(path, "r") as f:
     selector.select_and_visualize(f, tid, rr)
